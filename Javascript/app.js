64b65caa523d9598c8397bf66f524728d1d691db
Mridul2820 <immridulpanda2820@gmail.com>
2019-12-04 23:15:16 +0530

    footer redesign

--- Javascript/app.js
+++
@@ -1,6 +0,0 @@
-var container = document.querySelector('.grid-container');
-var navbar = document.querySelector('.nav-bar');
-
-container.addEventListener('scroll', function(){
-    navbar.style.color = "red";
-})
